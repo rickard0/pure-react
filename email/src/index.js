07fd0425a03357519dddc2a9b54bab6b89151104
rickard0 <br185076@ncr.com>
2017-08-08 20:18:31 -0700

    Add Email line item

---
+++ email/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function MailItem({message})
+{
+	const sender = message.sender;
+	const subject = message.subject;
+	const date = message.date;
+	const text = message.text;
+	return (
+	<div className="line">
+		<div className="icons">&nbsp;
+			<span className="checkbox">
+				<i className="fa fa-square-o fa-lg"/>
+			</span>
+			<span className="pin">
+				<i className="fa fa-thumb-tack fa-lg" />
+			</span>
+		</div>
+		<div className="text-items">
+			<div className="from-subject-line">
+				<span className="sender">{sender}</span>
+				<span className="subject">{subject}</span>
+			</div>
+			<div className="message-text">{text}</div>
+		</div>
+		<div className="date">
+		{date}
+		</div>
+	</div>
+	);
+}
+
+	var message = { 
+		sender: "React Newsletter",
+		subject: "React Newsletter - Issue 36",
+		date: "Jul 15",
+		text: "React Newsletter Issue 36 - July 15th 2016 Read this issue on the web http://reactjsnewsletter.com/issues/36?sid=3QGDYBX ### Comme"
+	};
+
+ReactDOM.render(<MailItem message={message} />, document.getElementById('root'));
